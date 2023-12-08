d113622a53ed1b7b012216e461d88e689b6063d1
YIHONG <akashidaiki2@gmail.com>
2023-12-08 15:51:51 +0800

    修改標題管理後台功能將圖片顯示在頁面上的功能

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 include_once "./api/db.php";
 ?>
-<!DOCTYPE html
-  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -30,7 +29,7 @@ include_once "./api/db.php";
     ?>
 
     <a title="$title['text']" href="index.php">
-      <div class="ti" style="background:url('use/'); background-size:cover;"></div>
+      <div class="ti" style="background:url('./img/<?= $title['img'] ?>'); background-size:cover;"></div>
       <!--標題-->
     </a>
     <div id="ms">
@@ -79,60 +78,56 @@ include_once "./api/db.php";
 
 
 
-      <div id="alt"
-        style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+      <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
       </div>
       <script>
-      $(".sswww").hover(
-        function() {
-          $("#alt").html("" + $(this).children(".all").html() + "").css({
-            "top": $(this).offset().top - 50
-          })
-          $("#alt").show()
-        }
-      )
-      $(".sswww").mouseout(
-        function() {
-          $("#alt").hide()
-        }
-      )
+        $(".sswww").hover(
+          function() {
+            $("#alt").html("" + $(this).children(".all").html() + "").css({
+              "top": $(this).offset().top - 50
+            })
+            $("#alt").show()
+          }
+        )
+        $(".sswww").mouseout(
+          function() {
+            $("#alt").hide()
+          }
+        )
       </script>
       <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
         <!--右邊-->
-        <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
-          onclick="lo('?do=login')">管理登入</button>
+        <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
         <div style="width:89%; height:480px;" class="dbor">
           <span class="t botli">校園映象區</span>
           <script>
-          var nowpage = 0,
-            num = 0;
+            var nowpage = 0,
+              num = 0;
 
-          function pp(x) {
-            var s, t;
-            if (x == 1 && nowpage - 1 >= 0) {
-              nowpage--;
+            function pp(x) {
+              var s, t;
+              if (x == 1 && nowpage - 1 >= 0) {
+                nowpage--;
+              }
+              if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+                nowpage++;
+              }
+              $(".im").hide()
+              for (s = 0; s <= 2; s++) {
+                t = s * 1 + nowpage * 1;
+                $("#ssaa" + t).show()
+              }
             }
-            if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
-              nowpage++;
-            }
-            $(".im").hide()
-            for (s = 0; s <= 2; s++) {
-              t = s * 1 + nowpage * 1;
-              $("#ssaa" + t).show()
-            }
-          }
-          pp(1)
+            pp(1)
           </script>
         </div>
       </div>
     </div>
     <div style="clear:both;"></div>
-    <<<<<<< HEAD <div
-      style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+    <<<<<<< HEAD <div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
       <span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom'] ?></span>
       =======
-      <div
-        style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+      <div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
         <span class="t" style="line-height:123px;"></span>
         >>>>>>> bbd490f8801e6016396f73daec01ed9d5bbcc70d
       </div>
