73c186d4d6fc74d99f37e3de239beb591a70e6d4
YIHONG <akashidaiki2@gmail.com>
2023-12-11 13:19:13 +0800

    完成news分頁功能，添加了上下頁的機制

--- back/news.php
+++ back/news.php
@@ -10,36 +10,70 @@
         </tr>
         <?php
 
-
-        $rows = $DB->all();
+        $total = $DB->count();
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        $rows = $DB->all(" limit $start, $div");
         foreach ($rows as $row) {
           # code...
 
         ?>
-          <tr>
-            <td>
-              <textarea type="text" name="text[<?= $row['id'] ?>]" id="" style="width:90%;height:60px;"><?= $row['text'] ?></textarea>
-              <!-- 這裡用直接塞 id 的方式取代 input:hidden 帶 id 的方式， -->
-            </td>
-            <td>
-              <input type="checkbox" name="sh[]" id="" value="<?= $row['id'] ?>" <?= ($row['sh'] == 1) ? 'checked' : '' ?>>
-            </td>
-            <td>
-              <input type="checkbox" name="del[]" id="" value="<?= $row['id'] ?>">
-            </td>
-          </tr>
+        <tr>
+          <td>
+            <textarea type="text" name="text[<?= $row['id'] ?>]" id=""
+              style="width:90%;height:60px;"><?= $row['text'] ?></textarea>
+            <!-- 這裡用直接塞 id 的方式取代 input:hidden 帶 id 的方式， -->
+          </td>
+          <td>
+            <input type="checkbox" name="sh[]" id="" value="<?= $row['id'] ?>"
+              <?= ($row['sh'] == 1) ? 'checked' : '' ?>>
+          </td>
+          <td>
+            <input type="checkbox" name="del[]" id="" value="<?= $row['id'] ?>">
+          </td>
+        </tr>
         <?php
         }
 
         ?>
       </tbody>
     </table>
+
+    <div class="cent">
+      <?php
+            if ($now > 1) {
+                $prev = $now - 1;
+
+                // 這裡用 &lt 的 html entity 來代表 [ < ] 
+                // echo "<a href='?do=news&p=$prev'> &lt </a>";
+                echo "<a href='?do=news&p=$prev'> < </a>";
+            }
+
+            for ($i = 1; $i <= $pages; $i++) {
+
+                $fontsize = ($now == $i) ? '24px' : '16px';
+
+                echo "<a href='?do=news&p=$i' style='font-size:$fontsize'> $i </a>";
+            }
+
+            if ($now < $pages) {
+                $next = $now + 1;
+                // echo "<a href='do=news&p=$next'> &gt </a>"
+                echo "<a href='?do=news&p=$next'> > </a>";
+            }
+
+            ?>
+    </div>
+
     <table style="margin-top:40px; width:70%;">
       <tbody>
         <tr>
           <input type="hidden" name="table" value="<?= $do ?>">
 
-          <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do ?>.php?table=<?= $do ?>')" value="新增最新消息資料">
+          <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do ?>.php?table=<?= $do ?>')"
+              value="新增最新消息資料">
           </td>
           <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
         </tr>
