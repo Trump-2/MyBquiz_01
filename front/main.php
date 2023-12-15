f4665b931bcb16cbe106c1a9c56c3199cc513d29
YIHONG <akashidaiki2@gmail.com>
2023-12-15 15:37:19 +0800

    完成彈出視窗內容顯示的設定

--- front/main.php
+++ front/main.php
@@ -42,13 +42,23 @@
 
   <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
     <span class="t botli">最新消息區
+      <?php
+      if ($News->count(['sh' => 1]) > 5) {
+        echo "<a href='?do=news' style='float:right'>More...</a>";
+      }
+
+      ?>
     </span>
     <ul class="ssaa" style="list-style-type:decimal;">
       <?php
+      // limit 前面要有空白；sql 語句裡面有空白；限制為 5 筆
       $news = $News->all(['sh' => 1], ' limit 5');
       foreach ($news as $new) {
         echo "<li>";
         echo mb_substr($new['text'], 0, 20);
+        echo "<div class='all' style='display:none'>";
+        echo $new['text'];
+        echo "</div>";
         echo "...</li>";
       }
 
@@ -59,6 +69,7 @@
     <script>
       $(".ssaa li").hover(
         function() {
+          // 73 行的 $this 指的是 hover 時的 li；整段的意思 li 底下的子容器的內容
           $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
           $("#altt").show()
         }
