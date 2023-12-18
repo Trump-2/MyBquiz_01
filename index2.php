75842768cce99b07c46f165e9d8f4109534c4d22
YIHONG <akashidaiki2@gmail.com>
2023-12-18 13:12:28 +0800

    調主次選單的呈現方式及css設定,完成主選單區功能

--- index2.php
+++ index2.php
@@ -1,4 +1,7 @@
-<?php include_once './api/db.php'; ?>
+<?php
+
+include_once "./api/db.php";
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -15,16 +18,18 @@
 <body>
   <div id="cover" style="display:none; ">
     <div id="coverr">
-      <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
+      <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('#cover')">X</a>
       <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
     </div>
   </div>
+  <iframe style="display:none;" name="back" id="back"></iframe>
   <div id="main">
     <?php
     $title = $Title->find(['sh' => 1]);
     ?>
-    <a title="<?= $title['text']; ?>" href="index.php">
-      <div class="ti" style="background:url(&#39;./img/<?= $title['img']; ?>&#39;); background-size:cover;"></div>
+
+    <a title="$title['text']" href="index.php">
+      <div class="ti" style="background:url('./img/<?= $title['img'] ?>'); background-size:cover;"></div>
       <!--標題-->
     </a>
     <div id="ms">
@@ -32,47 +37,24 @@
         <div id="menuput" class="dbor">
           <!--主選單放此-->
           <span class="t botli">主選單區</span>
-          <?php
-          $mainmu = $Menu->all(['sh' => 1, 'menu_id' => 0]);
-          foreach ($mainmu as $main) {
-          ?>
-            <div class='mainmu'>
-              <a href="<?= $main['href']; ?>" style="color:#000; font-size:13px; text-decoration:none;"><?= $main['text']; ?></a>
-              <?php
-
-              if ($Menu->count(['menu_id' => $main['id']]) > 0) {
-                echo "<div class='mw'>";
-                $subs = $Menu->all(['menu_id' => $main['id']]);
-                foreach ($subs as $sub) {
-                  echo "<a href='{$sub['href']}'>";
-                  echo "<div class='mainmu2'>";
-                  echo $sub['text'];
-                  echo "</div>";
-                  echo "</a>";
-                }
-                echo "</div>";
-              }
-              ?>
-
-            </div>
-
-            </a>
-          <?php
-          }
-          ?>
         </div>
         <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-          <span class="t">進站總人數 : <?= $Total->find(1)['total']; ?></span>
+          <span class="t">進站總人數 :<?= $Total->find(1)['total'] ?>
+          </span>
         </div>
       </div>
 
+      <!--這裡被剪下拆分出去了 -->
+
+      <!-- 判斷該引用哪個拆分出去的檔案，並顯示不同的頁面 -->
+
       <?php
+
 
       // $do = isset($_GET['page']) ? $_GET['page'] : 'main';
       // 上面那行的簡寫，使用 nullish coalescing operator；因為 isset( ) 的變數跟條件為 true 時選擇的值是一樣的
-
       $do = $_GET['do'] ?? 'main';
-      $file = "./front/{$do}.php";
+      $file = "./front/$do.php";
 
       // file_exist( ) 判斷檔案是否存在
       if (file_exists($file)) {
@@ -81,65 +63,80 @@
         include "./front/main.php";
       }
 
+      // switch ($do) {
+      //   case 'login':
+      //     include "./front/login.php";
+      //     break;
+      //   case 'new':
+      //     include "./front/new.php";
+      //     break;
+      //   default:
+      //     include "./front/main.php";
+      // }
       ?>
+
+
 
       <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
         <!--右邊-->
-        <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
+        <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
         <div style="width:89%; height:480px;" class="dbor">
           <span class="t botli">校園映象區</span>
-          <div class="cent" onclick="pp(1)"><img src="./icon/up.jpg" alt=""></div>
+          <div class="cent" onclick="pp(1)"><img src="./material/up.jpg" alt=""></div>
           <?php
+
           $imgs = $Image->all(['sh' => 1]);
 
           foreach ($imgs as $idx => $img) {
           ?>
-            <div id="ssaa<?= $idx; ?>" class='im cent'>
-              <img src="./img/<?= $img['img']; ?>" style="width:150px;height:103px;border:3px solid orange;margin:3px">
+            <div id="ssaa<?= $idx ?>" class="im cent">
+              <img src="./img/<?= $img['img'] ?>" alt="" style="width:150px;height:103px;border:5px solid brown; margin:2px;">
             </div>
           <?php
+
           }
           ?>
-          <div class="cent" onclick="pp(2)"><img src="./icon/dn.jpg" alt=""></div>
+          <div class="cent" onclick="pp(2)"><img src="./material/dn.jpg" alt=""></div>
           <script>
             // 手動改成 1
             var nowpage = 1,
               // 資料庫中的圖片數
-              num = <?= $Image->count(['sh' => 1]); ?>;
+              num = <?= $Image->count(['sh' => 1]) ?>;
 
             // 圖片換業的程式碼
-
             function pp(x) {
               var s, t;
-
 
               if (x == 1 && nowpage - 1 >= 0) {
                 nowpage--;
               }
-              // 在 php 中邏輯運算子和運算式相鄰時，建議將運算式用 () 包起來；在 js 沒這個問題
               if (x == 2 && (nowpage + 1) <= num * 1 - 3) {
                 nowpage++;
               }
+
+              /*
+                nowpage num s t
+
+
+
+              */
+
 
               $(".im").hide()
               for (s = 0; s <= 2; s++) {
                 t = s * 1 + nowpage * 1;
                 $("#ssaa" + t).show() // => 例如：#ssaa1、#ssaa2、#ssaa3
-
               }
             }
-
-
-            pp(2)
+            pp(1)
           </script>
         </div>
       </div>
     </div>
     <div style="clear:both;"></div>
     <div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-      <span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
+      <span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom'] ?></span>
     </div>
-  </div>
 
 </body>
 
